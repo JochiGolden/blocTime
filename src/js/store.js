82f8567aa4ed1e087a89f5b7b2c5a1fcccd5517b
JochiGolden <jacksonstruthers@gmail.com>
2016-08-21 17:51:26 +0200

    Reset timer store value

--- src/js/store.js
+++ src/js/store.js
@@ -7,11 +7,11 @@ import rootReducer from './reducers/index';
 const defaultState = {
   "setTime" : {
     "timerOptions" : {
-      "pomodoro" : { label : "Pomodoro", startTime : "0010" },
+      "pomodoro" : { label : "Pomodoro", startTime : "2500" },
       "shortBreak" : { label : "Short Break", startTime : "0500" },
       "longBreak" : { label : "Long Break", startTime : "3000" }
     },
-    "currentTime" : "0010",
+    "currentTime" : "2500",
     "countingDown" : false,
     "currentOption" : "pomodoro",
     "onBreak" : false,
